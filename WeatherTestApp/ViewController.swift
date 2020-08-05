101f3d4f429a8075eeb9b064b03402809c25c671
Arseny <kolosowski.a.r@gmail.com>
2020-08-05 14:01:41 +0300

    Initial Commit

---
+++ WeatherTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherTestApp
+//
+//  Created by User on 8/5/20.
+//  Copyright © 2020 vironIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
