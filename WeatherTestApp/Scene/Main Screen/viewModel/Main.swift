4008fec145e650d7dee345ad86f1dec6c3c713ed
Arseny <kolosowski.a.r@gmail.com>
2020-08-06 09:07:01 +0300

    feat: main view model

---
+++ WeatherTestApp/Scene/Main Screen/viewModel/Main.swift
@@ -0,0 +1,20 @@
+//
+//  Main.swift
+//  WeatherTestApp
+//
+//  Created by User on 8/6/20.
+//  Copyright © 2020 vironIT. All rights reserved.
+//
+
+import UIKit
+
+final class Main {
+    private let router: RouterProtocol
+    
+    init(router: RouterProtocol) { self.router = router }
+    
+    func push(_ configure: (UINavigationItem) -> Void) {
+        router.pushToTempController(configure)
+    }
+    
+}
