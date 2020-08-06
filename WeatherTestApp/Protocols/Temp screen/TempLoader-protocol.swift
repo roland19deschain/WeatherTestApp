b6d9fb419d9f51797b0f82d1edcaf553a4b02056
Arseny <kolosowski.a.r@gmail.com>
2020-08-06 09:30:24 +0300

    feat: protocol for temp view model

---
+++ WeatherTestApp/Protocols/Temp screen/TempLoader-protocol.swift
@@ -0,0 +1,15 @@
+//
+//  TempLoader-protocol.swift
+//  WeatherTestApp
+//
+//  Created by User on 8/6/20.
+//  Copyright © 2020 vironIT. All rights reserved.
+//
+
+import Foundation
+
+typealias WeatherResponse = (temp: String, description: String)
+
+protocol TempLoaderProtocol {
+    func loadTemp(city: String, successHandle: @escaping (WeatherResponse) -> Void, errorHandle: @escaping (Error) -> Void)
+}
