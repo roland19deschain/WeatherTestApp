5277fe3eb07e348799b70ebf2a4fa8ecd9ea6877
Arseny <kolosowski.a.r@gmail.com>
2020-08-07 14:50:17 +0300

    feat: added new json model and extend weather model

---
+++ WeatherTestApp/Models/Coord.swift
@@ -0,0 +1,13 @@
+//
+//  Coord.swift
+//  WeatherTestApp
+//
+//  Created by User on 8/7/20.
+//  Copyright © 2020 vironIT. All rights reserved.
+//
+
+import Foundation
+
+struct Coord: Codable {
+    let lon, lat: Double
+}
