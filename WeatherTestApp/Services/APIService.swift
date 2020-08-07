e0f44acd673aa1367c8b3c1530bec3d88ef34395
Arseny <kolosowski.a.r@gmail.com>
2020-08-07 14:10:26 +0300

    fix: deleted print line, added a response status check

--- WeatherTestApp/Services/APIService.swift
+++ WeatherTestApp/Services/APIService.swift
@@ -17,12 +17,20 @@ final class APIService {
     func rxResponse<T: Codable>(by urlString: String?, parameters: [String: Any],  _ completion: @escaping (Observable<T>?, Error?) -> Void) {
         guard let urlString = urlString, var components = URLComponents(string: urlString) else { return }
         components.queryItems = parameters.map({ URLQueryItem(name: $0, value: "\($1)") })
-        print(components.url!)
         _ = response(.get, components.url!.absoluteString)
             .subscribe({ response in
                 if let error = response.error {
                     completion(nil, error)
                     return
+                }
+                switch response.element?.statusCode ?? 0 {
+                case 400...499:
+                    let error = NSError(domain: "Client error", code: 400, userInfo: nil)
+                    completion(nil, error)
+                case 500...:
+                    let error = NSError(domain: "Server error", code: 500, userInfo: nil)
+                    completion(nil, error)
+                default: break
                 }
                 completion(decodable(.get, urlString), nil)
             })
