9ae368a04ef95d103603268b259820630b5493d9
Arseny <kolosowski.a.r@gmail.com>
2020-08-07 14:09:36 +0300

    fix: added s to http

--- WeatherTestApp/Constants/Constants.swift
+++ WeatherTestApp/Constants/Constants.swift
@@ -10,7 +10,7 @@ import Foundation
 
 struct Constants {
     static let apiKey = "526c5e3e34b1c0cc4046b54adebbac0a"
-    static let urlString = "http://api.openweathermap.org/data/2.5/weather"
+    static let urlString = "https://api.openweathermap.org/data/2.5/weather"
     static let kelvin = 273.5
     static let map = "MapKit"
     static let googleMap = "GoogleMaps"
