6761c4427bcc061f697fd229a5b53c796cbc3e49
Arseny <kolosowski.a.r@gmail.com>
2020-08-18 13:45:56 +0300

    refactor: created custom enum errors

--- WeatherTestApp/Services/APIService.swift
+++ WeatherTestApp/Services/APIService.swift
@@ -11,28 +11,35 @@ import RxSwift
 import RxAlamofire
 
 final class APIService {
+    enum NetworkError: String, Error {
+        case redirection = "Redirects occurred"
+        case client = "Check whether the input is correct"
+        case server = "Server error"
+        case other = "Oops, something went wrong"
+    }
+    
     static let shared = APIService()
     private init() {}
     
-    func rxResponse<T: Codable>(by urlString: String?, parameters: [String: Any],  _ completion: @escaping (Observable<T>?, Error?) -> Void) {
+    func rxResponse<T: Codable>(by urlString: String?, parameters: [String: Any],  _ completion: @escaping (Observable<T>?, NetworkError?) -> Void) {
         guard let urlString = urlString, var components = URLComponents(string: urlString) else { return }
         components.queryItems = parameters.map({ URLQueryItem(name: $0, value: "\($1)") })
         _ = response(.get, components.url!.absoluteString)
             .subscribe(onNext: { response in
                 
-                var error: NSError?
+                var error: NetworkError?
                 switch response.statusCode {
                 case 300...399:
-                    error = NSError(domain: "Redirects occurred", code: response.statusCode, userInfo: nil)
+                    error = .redirection
                 case 400...499:
-                    error = NSError(domain: "Check whether the input is correct", code: response.statusCode, userInfo: nil)
+                    error = .client
                 case 500...:
-                    error = NSError(domain: "Server error", code: response.statusCode, userInfo: nil)
+                    error = .server
                 default: break
                 }
                 completion(nil, error)
                 
-            }, onError: { error in completion(nil, error) },
+            }, onError: { _ in completion(nil, .other) },
                onCompleted: { completion(decodable(.get, components.url!.absoluteString), nil) })
     }
     
