2165c8c6cfd52d667756af9d6ae4a5057bd32536
Arseny <kolosowski.a.r@gmail.com>
2020-08-31 12:34:53 +0300

    fix: adding mapping

---
+++ WeatherTestApp/Extensions/History+Mapping.swift
@@ -0,0 +1,19 @@
+//
+//  History+Mapping.swift
+//  WeatherTestApp
+//
+//  Created by User on 8/31/20.
+//  Copyright © 2020 vironIT. All rights reserved.
+//
+
+import Foundation
+
+extension History: ObjectProtocol {
+    func toMappedObject<T>() -> T? where T : MappedObjectProtocol {
+        CDHistory(date: date!,
+                  city: city ?? "",
+                  temp: temp ?? "",
+                  weather: weather ?? "",
+                  identifier: objectID.uriRepresentation().absoluteString) as? T
+    }
+}
